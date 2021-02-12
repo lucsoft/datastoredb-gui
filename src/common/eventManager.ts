1b6b25e3f654eebecb58c3bc1a08ef9f8f8d201d
lucsoft <mail@lucsoft.de>
2021-02-12 18:50:30 +0100

    Created a eventManager

---
+++ src/common/eventManager.ts
@@ -0,0 +1,23 @@
+export enum DataStoreEvents
+{
+    IncidentBar,
+    RefreshData,
+    RefreshDataComplete
+}
+
+const events: DataStoreEvent[] = []
+
+type DataStoreEvent = {
+    id: DataStoreEvents
+    action: (metaData: any) => void
+}
+
+export const registerEvent = (id: DataStoreEvents, action: (metaData: any) => void) =>
+{
+    events.push({ id, action })
+}
+
+export const emitEvent = (id: DataStoreEvents, metaData: any) =>
+{
+    events.filter(x => x.id === id).forEach(x => x.action(metaData))
+}
