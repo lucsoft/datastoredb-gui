a537ae68987011eaf5a62df585383515b7d4d2f3
lucsoft <mail@lucsoft.de>
2021-02-13 13:58:19 +0100

    Version Bump

--- webpack.config.js
+++ webpack.config.js
@@ -4,70 +4,66 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const path = require('path');
 
-module.exports = {
-    entry: {
-        index: "./src/index.ts"
-    },
-    mode: "development",
-    devtool: 'inline-source-map',
-    output: {
-        filename: '[name].js',
-        chunkFilename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-    },
-    resolve: {
-        extensions: [ ".js", ".ts" ]
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(png|jpe?g|gif)$/i,
-                use: [
-                    {
-                        loader: 'file-loader',
-                    },
-                ],
-            },
-            {
-                test: /\.ts$/,
-                loader: "ts-loader"
-            },
-            {
-                test: /\.css$/i,
-                use: [
-                    {
-                        loader: MiniCssExtractPlugin.loader
-                    },
-                    'css-loader'
-                ]
-            },
-            {
-                test: /\.svg$/i,
-                use: [ 'url-loader' ]
-            }
-        ]
-    },
-    devServer: {
-        contentBase: "./dist",
-        port: 80,
-        host: '0.0.0.0'
-    },
-    plugins: [
-        new MiniCssExtractPlugin({
-            filename: '[name].css',
-            chunkFilename: '[id].css'
-        }),
-        new CleanWebpackPlugin(),
-        new HtmlWebpackPlugin({
-            inject: 'body',
-            chunks: [ 'index' ],
-            template: './src/index.html',
-            filename: 'index.html',
-            minify: { minifyCSS: true, minifyJS: true, removeComments: true }
-        }),
-    ],
-    optimization: {
-        minimize: true,
-        minimizer: [ new CssMinimizerPlugin() ]
+module.exports = (_, mode) =>
+{
+    const isProduction = (typeof mode.env.production === "boolean" && mode.env.production);
+    return {
+        entry: {
+            index: "./src/index.ts"
+        },
+        mode: isProduction ? "production" : "development",
+        devtool: isProduction ? undefined : 'inline-source-map',
+        output: {
+            filename: '[name].js',
+            chunkFilename: '[name].bundle.js',
+            path: path.resolve(__dirname, 'dist'),
+        },
+        resolve: {
+            extensions: [ ".js", ".ts" ]
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.(png|jpe?g|gif|svg)$/i,
+                    use: 'file-loader'
+                },
+                {
+                    test: /\.ts$/,
+                    loader: "ts-loader"
+                },
+                {
+                    test: /\.css$/i,
+                    use: [
+                        {
+                            loader: MiniCssExtractPlugin.loader
+                        },
+                        'css-loader'
+                    ]
+                }
+            ]
+        },
+        devServer: {
+            contentBase: "./dist",
+            port: 80,
+            host: '0.0.0.0'
+        },
+        plugins: [
+            new MiniCssExtractPlugin({
+                filename: '[name].css',
+                chunkFilename: '[id].css'
+            }),
+            new CleanWebpackPlugin(),
+            new HtmlWebpackPlugin({
+                inject: 'body',
+                chunks: [ 'index' ],
+                template: './src/index.html',
+                filename: 'index.html',
+                minify: isProduction ? { minifyCSS: true, minifyJS: true, removeComments: true } : undefined
+            }),
+        ],
+        optimization: isProduction ? {
+            minimize: true,
+            minimizer: [ new CssMinimizerPlugin() ]
+        } : undefined
     }
-};
+}
