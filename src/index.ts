e474206547973cd9cd8f86c9b12c67563427d891
lucsoft <mail@lucsoft.de>
2021-03-14 00:13:05 +0100

    Forced White mode (for debugging)

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { WebGen } from "@lucsoft/webgen";
-const web = new WebGen()
+import { SupportedThemes, WebGen } from "@lucsoft/webgen";
+const web = new WebGen({ theme: SupportedThemes.white })
 
 import('./dashboard').then(x => x.renderMain(web))
