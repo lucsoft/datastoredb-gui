f57f3c1e044dbb018b2dd11f6802c44999e95ad0
lucsoft <mail@lucsoft.de>
2021-04-07 23:19:55 +0200

    Added array compare for recommendations for variants

--- src/common/arrayCompare.ts
+++ src/common/arrayCompare.ts
@@ -1,5 +1,8 @@
 export const compareArray = (allEntries: string[], requiredEntries: string[]) =>
     allEntries.filter((v) => requiredEntries.map(x => x.toLowerCase()).includes(v.toLowerCase())).length === requiredEntries.length;
 
+export const compareArrayHalfMatch = (allEntries: string[], requiredEntries: string[]) =>
+    allEntries.filter((v) => requiredEntries.map(x => x.toLowerCase()).includes(v.toLowerCase())).length >= requiredEntries.length / 2;
+
 export const execludeCompareArray = (allEntries: string[], requiredEntries: string[]) =>
     allEntries.filter((v) => requiredEntries.map(x => x.toLowerCase()).includes(v.toLowerCase())).length == 0 ? true : false;
