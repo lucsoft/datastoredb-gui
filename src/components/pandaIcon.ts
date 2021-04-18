3b2f02f79ae21ee233b595a2c7c82b139e434591
lucsoft <mail@lucsoft.de>
2021-04-18 17:52:35 +0200

    Fixed Panda Icon keep loading

--- src/components/pandaIcon.ts
+++ src/components/pandaIcon.ts
@@ -1,14 +1,13 @@
 import { RenderElement, span } from "@lucsoft/webgen";
 import pandaIcon from '../../res/pandaicon.svg';
 
+const data = fetch(pandaIcon).then(x => x.text());
+
 export const PandaIcon = (): RenderElement => {
-
     const icon = span(undefined, 'webgen-svg', 'panda-icon')
-    fetch(pandaIcon).then(x => x.text())
-        .then(x => {
-            icon.innerHTML = x
-        })
-
+    data.then(x => {
+        icon.innerHTML = x
+    })
     return {
         draw: () => {
             return icon;
